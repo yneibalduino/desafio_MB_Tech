9614d0071ead5ef67e85289ae147a9810677e296
Ynei Francisco Balduino <yfbalduino@gmail.com>
2022-10-18 14:58:59 -0300

    feat: button component

---
+++ src/components/button/styles.ts
@@ -0,0 +1,23 @@
+import styled, { css } from "styled-components/native";
+
+export type ButtonTypeStyleProps = 'PRIMARY' | 'SECONDARY';
+
+type Props = {
+  type: ButtonTypeStyleProps;
+}
+
+export const Container = styled.TouchableOpacity<Props>`
+  flex: 1;
+  min-height: 56px;
+  max-height: 56px;
+  background-color: ${({theme, type}) => type === 'PRIMARY' ? theme.COLORS.BLUE : theme.COLORS.RED};
+  border-radius: 25px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-size: ${({theme}) =>theme.FONT_SIZE.MD}px;
+  font-family: ${({theme}) =>theme.FONT_FAMILY.BOLD_ITALIC};
+  color: ${({theme}) =>theme.COLORS.WHITE};
+`;

730cb6d34f0920d0631fc374339bd5b41812ccfa
Ynei Francisco Balduino <yfbalduino@gmail.com>
2022-10-18 18:24:36 -0300

    feat: component EventText

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components/native';
+
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { EventText } from '../../components/EventText';
+
+
+export const Container = styled(SafeAreaView)`
+  flex: 1;
+  padding: 22px;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  justify-content: space-around;
+`;
+
+export const InfoWrapper = styled.View`
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Logo = styled.Image`
+  width: 216px;
+  height: 216px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${({theme}) => theme.FONT_SIZE.XL}px;
+  font-family: ${({theme}) =>theme.FONT_FAMILY.BOLD_ITALIC};
+
+  text-align: center;
+
+  width: 80%;
+
+  margin-top: 28px;
+  margin-bottom: 16px;
+`;
+
+export const SubTitle = styled(EventText)`
+  font-size: 16px;
+  color: ${({theme}) => theme.COLORS.GRAY_DARK};
+`;

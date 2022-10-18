676cf3de389b4bb56bb8059f000973bbedd740d9
Ynei Francisco Balduino <yfbalduino@gmail.com>
2022-10-18 18:31:29 -0300

    feat: component EventText

---
+++ src/components/EventText/index.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+
+export const EventText = styled.Text`
+  font-family: ${({theme}) => theme.FONT_FAMILY.REGULAR};
+  font-size: ${({theme}) => theme.FONT_SIZE.LG}px;
+`;

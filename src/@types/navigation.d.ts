37be3e4fdd1b05348940f4e63b5aa67348e1a60e
Ynei Francisco Balduino <yfbalduino@gmail.com>
2022-10-18 16:55:47 -0300

    feat: setup Navigation

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,17 @@
+export declare global{
+  namespace ReactNaviGation {
+    interface RootParamList {
+      home: undefined;
+      eventList: undefined;
+      eventDetails: {
+        eventCard: string;
+      }
+      checkout: {
+        eventCard: string;
+      }
+      payment: {
+        eventCard: string;
+      }
+    }
+  }
+}

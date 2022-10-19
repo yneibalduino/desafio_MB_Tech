8dfcb8c256475fd3f27266f24d122759190458f8
Ynei Francisco Balduino <yfbalduino@gmail.com>
2022-10-19 15:03:43 -0300

    feat: component EventCard

--- src/components/EventCard/index.tsx
+++ src/components/EventCard/index.tsx
@@ -1,17 +1,37 @@
-import { Container, SubTitle, Title } from "./styles";
+import { Text } from "react-native";
+import { Container, Content, DateAndHour, EventName, Participants } from "./styles";
 
-export function EventCard(){
+type Props = {
+  eventname?: string;
+  dateandhour?: string;
+  participants?: number;
+}
+
+export function EventCard({eventname, dateandhour, participants}: Props){
   return(
     <Container>
-      <Title>
-        Evento A
-      </Title>
-      <SubTitle>
-        Data e hora
-      </SubTitle>
-      <SubTitle>
-        Número de Participantes
-      </SubTitle>
+      <EventName>
+        {eventname}
+      </EventName>
+
+      <Content>
+        <DateAndHour>
+          Data e hora
+        </DateAndHour>
+        <DateAndHour>
+          {dateandhour}
+        </DateAndHour>
+      </Content>
+
+      <Content>
+        <Participants>
+          Número de participantes
+        </Participants>
+        <Participants>
+          {participants}
+        </Participants>
+      </Content>
+
     </Container>
   )
 }
--- src/components/EventCard/styles.ts
+++ src/components/EventCard/styles.ts
@@ -3,19 +3,31 @@ import styled from "styled-components/native";
 export const Container = styled.TouchableOpacity`
   border-radius: 10px;
   padding: 10px;
-  margin-bottom: 22px;
+  margin-bottom: 10px;
   background-color: ${({theme}) => theme.COLORS.GRAY_LIGHT};
 `;
 
-export const Title = styled.Text`
+export const Content = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const EventName = styled.Text`
   font-size: ${({theme}) =>theme.FONT_SIZE.LG}px;
   font-family: ${({theme}) =>theme.FONT_FAMILY.BOLD};
   color: ${({theme}) =>theme.COLORS.GRAY_DARK};
 `;
 
-export const SubTitle = styled.Text`
+export const DateAndHour = styled.Text`
   font-size: ${({theme}) =>theme.FONT_SIZE.SM}px;
   font-family: ${({theme}) =>theme.FONT_FAMILY.REGULAR};
   color: ${({theme}) =>theme.COLORS.GRAY_DARK};
   margin-top: 5px
+`;
+
+export const Participants = styled.Text`
+  font-size: ${({theme}) =>theme.FONT_SIZE.SM}px;
+  font-family: ${({theme}) =>theme.FONT_FAMILY.REGULAR};
+  color: ${({theme}) =>theme.COLORS.GRAY_DARK};
+  margin-top: 5px;
 `;

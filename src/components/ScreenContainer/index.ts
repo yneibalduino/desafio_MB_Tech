5799e88ae182b1fb9424bd09a4a37a84285ce6ed
Ynei Francisco Balduino <yfbalduino@gmail.com>
2022-10-20 20:22:06 -0300

    feat: screen container component

---
+++ src/components/ScreenContainer/index.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+
+export const ScreenContainer = styled.ScrollView.attrs({
+  contentContainerStyle: { padding: 24, flexGrow: 1 },
+  showsVerticalScrollIndicator: false,
+})``;

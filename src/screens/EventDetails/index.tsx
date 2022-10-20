c91f862d5d9590f7e58d8c8262382e6491d0639e
Ynei Francisco Balduino <yfbalduino@gmail.com>
2022-10-19 23:39:59 -0300

    fix: Event Details: navigation, route, button

--- src/screens/EventDetails/index.tsx
+++ src/screens/EventDetails/index.tsx
@@ -1,17 +1,25 @@
-import { RouteProp, useRoute } from '@react-navigation/native';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { View } from 'react-native';
 
 import { Routes } from '../../@types/navigation';
+import { Button } from '../../components/Button';
 import { EventCard } from '../../components/EventCard';
 import { Header } from '../../components/Header';
 import { Highlight } from '../../components/Highlight';
-import { Container } from './styles';
+import { ButtonContent, Container, Content, Text } from './styles';
 
 export function EventDetails() {
+  const navigation = useNavigation();
+
   const {
     params: { event },
   } = useRoute<RouteProp<Routes, 'eventDetails'>>();
-  console.log(event);
+
+  function handleCheckout(pressedEvent: Event) {
+    navigation.navigate('checkout', {
+      event: pressedEvent,
+    });
+  }
 
   return (
     <Container>
@@ -20,7 +28,19 @@ export function EventDetails() {
       </View>
       <Highlight title="Detalhes do Evento" isEventList={false} />
       <EventCard {...event} disabled />
-      <View />
+      <Content>
+        <Text>Regras do evento:</Text>
+        <Text>Traje livre (no dress code).</Text>
+        <Text>Permitida a entrada de pets.</Text>
+        <Text>Proibida a entrada de qualquer tipo de comida ou bebida.</Text>
+        <Text>Evento livre para todas as idades.</Text>
+      </Content>
+      <ButtonContent>
+        <Button
+          title="Comprar ingressos"
+          onPress={() => handleCheckout(event)}
+        />
+      </ButtonContent>
     </Container>
   );
 }
--- src/screens/EventDetails/styles.ts
+++ src/screens/EventDetails/styles.ts
@@ -3,5 +3,23 @@ import styled from 'styled-components/native';
 
 export const Container = styled(SafeAreaView)`
   flex: 1;
-  padding: 24px;
+  padding: 22px;
+`;
+
+export const Content = styled.View`
+  border-radius: 10px;
+  padding: 10px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_LIGHT};
+  margin-top: 10px; ;
+`;
+
+export const ButtonContent = styled.View`
+  flex: 1;
+  justify-content: space-around;
+`;
+
+export const Text = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  color: ${({ theme }) => theme.COLORS.GRAY_DARK};
 `;

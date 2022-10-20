2d35fe00f6962fb1b08681f37cf1783644904cf4
Ynei Francisco Balduino <yfbalduino@gmail.com>
2022-10-19 23:41:15 -0300

    fix: Event List: navigation, useEffect, useState

--- src/screens/EventList/index.tsx
+++ src/screens/EventList/index.tsx
@@ -1,11 +1,46 @@
+import { useEffect, useState } from 'react';
+
+import { useNavigation } from '@react-navigation/native';
 import { View, FlatList } from 'react-native';
 
 import { EventCard } from '../../components/EventCard';
+import { Event } from '../../components/EventCard/types';
 import { Header } from '../../components/Header';
 import { Highlight } from '../../components/Highlight';
 import { Container } from './styles';
 
 export function EventList() {
+  const navigation = useNavigation();
+  const [events, setEvents] = useState<Event[]>([]);
+
+  function handleEventDetails(pressedEvent: Event) {
+    navigation.navigate('eventDetails', {
+      event: pressedEvent,
+    });
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setEvents([
+        {
+          eventName: 'Marejada',
+          dateAndHour: '10/10/2022',
+          participants: 670,
+        },
+        {
+          eventName: 'Octoberfest',
+          dateAndHour: '15/10/2022',
+          participants: 893,
+        },
+        {
+          eventName: 'Volvo Ocean Race',
+          dateAndHour: '23/11/2022',
+          participants: 752,
+        },
+      ]);
+    }, 1000);
+  }, []);
+
   return (
     <Container>
       <View>
@@ -17,25 +52,11 @@ export function EventList() {
         isEventList={true}
       />
       <FlatList
-        data={[
-          {
-            eventName: 'Marejada',
-            dateAndHour: '10/10/2022',
-            participants: 670,
-          },
-          {
-            eventName: 'Octoberfest',
-            dateAndHour: '15/10/2022',
-            participants: 893,
-          },
-          {
-            eventName: 'Volvo Ocean Race',
-            dateAndHour: '23/11/2022',
-            participants: 752,
-          },
-        ]}
-        keyExtractor={item => item.eventName}
-        renderItem={({ item }) => <EventCard {...item} />}
+        data={events}
+        keyExtractor={event => event.eventName}
+        renderItem={({ item: event }) => (
+          <EventCard {...event} onPress={() => handleEventDetails(event)} />
+        )}
         showsVerticalScrollIndicator={false}
       />
     </Container>
